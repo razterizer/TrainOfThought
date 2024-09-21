56585760aff002db2fd79043b4a9f42babdec042
Rasmus Anthin <scratty@gmail.com>
2024-09-21 03:01:11 +0200

    Bootstrapping unit tests in main() of unit_tests.cpp.

--- Tests/unit_tests.cpp
+++ Tests/unit_tests.cpp
@@ -5,4 +5,10 @@
 //  Created by Rasmus Anthin on 2024-09-21.
 //
 
-#include "unit_tests.hpp"
+#include "MachineLearning_tests.h"
+
+int main(int argc, char** argv)
+{
+  ml::ann::unit_tests();
+  return 0;
+}
